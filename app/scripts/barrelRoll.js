9dd5652d75939dbb0f7c6a463d7f1b963bb272b2
duncky <d@duncky.com>
2013-12-09 22:54:57 +0000

    add css to rotate, inject css onto page

---
+++ app/scripts/barrelRoll.js
@@ -0,0 +1,28 @@
+'use strict';
+(function ($) {
+  $(document).ready(function(){
+    var minTimeRange = 1000*60;
+    var maxTimeRange = 1000*60*10;
+
+    setInterval(function() {
+      console.log('trying...');
+      injectCSS();
+    }, randomise(minTimeRange,maxTimeRange));
+  });
+
+  function injectCSS() {
+    var path = chrome.extension.getURL('styles/main.css');
+    inject(path);
+  }
+
+  function inject(path) {
+    $('head').append($('<link>')
+      .attr('rel','stylesheet')
+      .attr('type','text/css')
+      .attr('href', path));
+  }
+
+  function randomise(max,min) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+})(jQuery);
